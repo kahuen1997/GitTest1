ee9e0d503cb2c440a2a752d42adbf950f0ea1981
kahuen1997 <kahuen1997.career@gmail.com>
2023-04-10 15:32:23 +0800

    This is the first GitHub SourceTree Test.

---
+++ src/test/LaunchApp.java
@@ -0,0 +1,9 @@
+package test;
+
+public class LaunchApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int S = 8;
+        System.out.println(S);
+    }
+}
