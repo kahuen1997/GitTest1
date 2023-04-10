b7dfb910ad03f3c1a6f4e9b39699be8c539873af
kahuen1997 <kahuen1997.career@gmail.com>
2023-04-10 15:42:33 +0800

    This is the Second Git Commit.

--- src/test/LaunchApp.java
+++ src/test/LaunchApp.java
@@ -4,6 +4,7 @@ public class LaunchApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
         int S = 8;
-        System.out.println(S);
+        int B = 9;
+        System.out.println(S + B);
     }
 }
