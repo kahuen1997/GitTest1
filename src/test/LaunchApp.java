adea54ba6c29475b549a069f0d705355ab483f37
kahuen1997 <kahuen1997.career@gmail.com>
2023-04-11 01:05:02 +0800

    This is the fourth change.

--- src/test/LaunchApp.java
+++ src/test/LaunchApp.java
@@ -1,15 +1,20 @@
 package test;
 
 public class LaunchApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int S = 8;
-        int B = 9;
-        System.out.println(S + B);
-        if (B > S) {
-            System.out.println("Correct");
-        } else {
-            System.out.println("Not Correct");
+        try{
+            System.out.println("Hello World");
+            int S = 8;
+            int B = 9;
+            System.out.println(S + B);
+            if (B > S) {
+                System.out.println("Correct");
+            } else {
+                System.out.println("Not Correct");
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong code.");
         }
     }
 }
